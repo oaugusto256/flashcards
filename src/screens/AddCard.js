c2782bfba4cf656f3ea2c498fb1d5e839019ca9d
tavioalves <tavioalves@gmail.com>
2019-02-05 10:50:32 -0200

    refactor: Take off setState function at render in AddCard screen

--- src/screens/AddCard.js
+++ src/screens/AddCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Text,
   View,
@@ -6,79 +6,81 @@ import {
   StatusBar,
   StyleSheet,
   Dimensions,
-  TouchableOpacity,
-} from 'react-native';
-import EmphasizeText from '../components/EmphasizeText';
-import { connect } from 'react-redux';
-import { addCard } from '../actions';
+  TouchableOpacity
+} from "react-native";
+import EmphasizeText from "../components/EmphasizeText";
+import { connect } from "react-redux";
+import { addCard } from "../actions";
 
 class AddCard extends Component {
   static navigationOptions = {
-    title: 'Add Card',
+    title: "Add Card",
     headerStyle: {
-      backgroundColor: '#393e46',
+      backgroundColor: "#393e46"
     },
-    headerTintColor: '#fff',
+    headerTintColor: "#fff",
     headerTitleStyle: {
-      fontWeight: 'bold',
-    },
+      fontWeight: "bold"
+    }
   };
 
   state = {
-    answer: '',
-    question: '',
-  }
+    answer: "",
+    question: ""
+  };
 
   onCreate = () => {
-    if(this.state.question !== '' && this.state.answer) {
+    if (this.state.question !== "" && this.state.answer) {
       const card = {
         question: this.state.question,
         answer: this.state.answer
-      }
+      };
 
-      const deckId = this.props.navigation.getParam('id', 'DEFAULT_VALUE');
+      const deckId = this.props.navigation.getParam("id", "DEFAULT_VALUE");
 
       this.props.addCard(card, deckId);
-      this.props.navigation.navigate('Deck', { deckId });
+      this.props.navigation.navigate("Deck", { deckId });
     }
-  }
+  };
+
+  handleChange = ({ question, answer }) => {
+    if (question) this.setState({ question });
+    else if (answer) this.setState({ answer });
+  };
 
   render() {
     return (
       <>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="#393e46"
-        />
+        <StatusBar barStyle="light-content" backgroundColor="#393e46" />
         <View style={styles.screen}>
           <View style={styles.content}>
             <Text style={styles.introText}>First,</Text>
-            <Text style={styles.text}>What is the <EmphasizeText text={'question'} />?</Text>
-            <Text style={styles.inputLabel}>
-              Card' question:
+            <Text style={styles.text}>
+              What is the <EmphasizeText text={"question"} />?
             </Text>
+            <Text style={styles.inputLabel}>Card' question:</Text>
             <TextInput
               style={styles.input}
               value={this.state.question}
               placeholder={"Enter the card' question"}
-              onChangeText={(question) => this.setState({ question })}
+              onChangeText={question => this.handleChange({ question })}
             />
-            <Text style={styles.inputLabel}>
-              Card' answer:
-            </Text>
+            <Text style={styles.inputLabel}>Card' answer:</Text>
             <TextInput
               style={styles.input}
               value={this.state.answer}
               placeholder={"Enter the card' answer"}
-              onChangeText={(answer) => this.setState({ answer })}
+              onChangeText={answer => this.handleChange({ answer })}
             />
             <TouchableOpacity
-              style={this.state.question.length >= 3 && this.state.answer.length >= 3  ? styles.buttonAdd : styles.buttonAddDisable}
+              style={
+                this.state.question.length >= 3 && this.state.answer.length >= 3
+                  ? styles.buttonAdd
+                  : styles.buttonAddDisable
+              }
               onPress={this.onCreate}
             >
-              <Text style={styles.buttonAddText}>
-                Add Card
-              </Text>
+              <Text style={styles.buttonAddText}>Add Card</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -91,38 +93,41 @@ const mapStateToProps = state => {
   return {
     decks: state.deck.decks,
     error: state.deck.error,
-    loading: state.deck.loading,
+    loading: state.deck.loading
   };
 };
 
-export default connect(mapStateToProps, {
-  addCard
-})(AddCard);
+export default connect(
+  mapStateToProps,
+  {
+    addCard
+  }
+)(AddCard);
 
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    height: height,
+    height: height
   },
   introText: {
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
-    marginBottom: 10,
+    marginBottom: 10
   },
   text: {
     fontSize: 16,
     marginTop: 10,
-    marginBottom: 20,
+    marginBottom: 20
   },
   content: {
     flex: 1,
-    padding: 15,
+    padding: 15
   },
   inputLabel: {
-    fontSize: 14,
+    fontSize: 14
   },
   input: {
     marginTop: 5,
@@ -132,11 +137,11 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     borderRadius: 3,
     borderWidth: 0.75,
-    borderColor: '#eeeeee',
+    borderColor: "#eeeeee"
   },
   buttonCreateContainer: {
     flex: 1,
-    flexDirection: 'column-reverse',
+    flexDirection: "column-reverse"
   },
   buttonAdd: {
     height: 50,
@@ -144,13 +149,13 @@ const styles = StyleSheet.create({
     marginTop: 15,
     borderRadius: 3,
     marginBottom: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#00adb5',
-    shadowColor: '#000',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00adb5",
+    shadowColor: "#000",
     shadowOpacity: 0.35,
-    justifyContent: 'center',
-    shadowOffset: { width: 0, height: 2 },
+    justifyContent: "center",
+    shadowOffset: { width: 0, height: 2 }
   },
   buttonAddDisable: {
     height: 50,
@@ -158,15 +163,15 @@ const styles = StyleSheet.create({
     marginTop: 15,
     borderRadius: 3,
     marginBottom: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#777',
-    shadowColor: '#000',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#777",
+    shadowColor: "#000",
     shadowOpacity: 0.35,
-    justifyContent: 'center',
-    shadowOffset: { width: 0, height: 2 },
+    justifyContent: "center",
+    shadowOffset: { width: 0, height: 2 }
   },
   buttonAddText: {
-    color: '#fff'
+    color: "#fff"
   }
-})
+});
